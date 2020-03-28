2751ab4dc7cc0aea505ef63bac5218dc1e33de83
franckysolo <franckysolo@gmail.com>
2020-03-28 19:39:01 +0100

    fix:(build) core.js warnings

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,24 @@
 const webpack = require('webpack')
 const path = require('path')
+const pattern =  /\@angular(\\|\/)core(\\|\/)fesm5/
+const sourceDirectory = path.join(__dirname, 'src')
 
 module.exports = {
   configureWebpack: {
-   plugins: [
-     new webpack.ContextReplacementPlugin(
-       /\@angular(\\|\/)core(\\|\/)esm5/,
-       path.join(__dirname, './src'))
-   ]
- }
+      plugins: [
+        new webpack.ContextReplacementPlugin(pattern, sourceDirectory)
+      ]
+   },
+   chainWebpack: config => {
+     config.module
+       .rule('angular-core-js')
+       .test(/[\/\\]@angular[\/\\].+\.js$/)
+       .use('cache-loader')
+       .loader('cache-loader')
+       .end()
+       .use('babel-loader')
+       .loader('babel-loader')
+       .end()
+       .parser({ system: true })
+   }
 }
